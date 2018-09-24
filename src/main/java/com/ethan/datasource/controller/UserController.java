88ab1ae58076c29f934ba2ce2e2da7539b92bb2e
wcg2018 <wyd790108961@gmail.com>
2018-09-24 23:54:47 +0800

    update insertUser method

--- src/main/java/com/ethan/datasource/controller/UserController.java
+++ src/main/java/com/ethan/datasource/controller/UserController.java
@@ -18,7 +18,7 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-    @PostMapping(name = "/add")
+    @PostMapping
     public String insertUser(String name, String password){
         User user = new User();
         user.setUsername(name);
